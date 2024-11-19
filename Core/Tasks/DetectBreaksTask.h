b47bfbae6e6e9b14477762bab995ed3ddd8f000e
Temberlane <60454907+Temberlane@users.noreply.github.com>
2024-11-19 15:03:44 -0500

    added detectbreakstask

---
+++ Core/Tasks/DetectBreaksTask.h
@@ -0,0 +1,11 @@
+//
+// Created by Thomas Li on 2024-11-12.
+//
+
+#ifndef UOSM_TELEMETRY_PICO_DETECTBREAKS_H
+#define UOSM_TELEMETRY_PICO_DETECTBREAKS_H
+
+void InitBreakSensorTask();
+void DetectBreaksTask();
+
+#endif //UOSM_TELEMETRY_PICO_DETECTBREAKS_H
