9edcf8c90fdac523b9f045d5ecde5bcfc63478c1
Temberlane <60454907+Temberlane@users.noreply.github.com>
2025-01-14 13:13:44 -0500

    Update interrupts.c
    sending signal for break interupt to the CAN

--- Core/Src/interrupts.c
+++ Core/Src/interrupts.c
@@ -5,6 +5,7 @@
 #include "interrupts.h"
 #include "ApplicationTypes.h"
 #include "data_aggregator.h"
+#include "CANDriver.h"
 
 /**
  * Brakes are connected to GPIO 0
@@ -12,6 +13,24 @@
 const uint BRAKE_PIN = 0;
 
 void brakes_callback(uint gpio, uint32_t events) {
+    bool brake_pressed = (events & GPIO_IRQ_EDGE_RISE);
+    data_aggregator_set_breaks_pressed(brake_pressed);
+    iCommsMessage_t brakeMsg;
+
+    // Sending the brake signal through CAN
+    if (brake_pressed) {
+        brakeMsg.standardMessageID = 0x123; // Brake message ID
+        brakeMsg.dataLength = 1;
+        brakeMsg.data[0] = brake_pressed;
+
+        uint8_t result = CANSPI_Transmit(&brakeMsg);
+
+        if (result == 1) {
+            DebugPrint("Brake signal sent successfully\n");
+        } else {
+            DebugPrint(("Brake signal sent failed\n"));
+        }
+
     data_aggregator_set_breaks_pressed(events & GPIO_IRQ_EDGE_RISE);
 }
 
