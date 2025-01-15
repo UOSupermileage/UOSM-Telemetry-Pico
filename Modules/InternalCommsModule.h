78bd0d54f0a66420e4f21fd2cc7fdc56eda4d969
Jeremy <jeremymcote@gmail.com>
2025-01-15 14:23:28 -0500

    Squashed 'UOSM-Core/' changes from c819e0d..cb61314
    cb61314 Merge UOSM-Core
    b06b36d Enable RTC for backup registers, cleanup CAN code
    
    git-subtree-dir: UOSM-Core
    git-subtree-split: cb613144bf45496d67fd95d28f5a3e223a641b18

--- Modules/InternalCommsModule.h
+++ Modules/InternalCommsModule.h
@@ -14,35 +14,44 @@ extern "C" {
 
 #include "ApplicationTypes.h"
 
-PUBLIC result_t IComms_Init();
+result_t IComms_Init();
 
-PUBLIC result_t IComms_ReceiveNextMessage(iCommsMessage_t* rxMsg);
+result_t IComms_ReceiveNextMessage(iCommsMessage_t* rxMsg);
 
-PUBLIC void IComms_PeriodicReceive();
+void IComms_PeriodicReceive();
 
-PUBLIC result_t IComms_Transmit(iCommsMessage_t* txMsg);
+result_t IComms_Transmit(iCommsMessage_t* txMsg);
 
-PUBLIC iCommsMessage_t IComms_CreateMessage(uint16_t standardMessageID, uint8_t dataLength, uint8_t data[8]);
+iCommsMessage_t IComms_CreateMessage(uint16_t standardMessageID, uint8_t dataLength, uint8_t data[8]);
 
-PUBLIC iCommsMessage_t IComms_CreatePercentageMessage(uint16_t standardMessageID, percentage_t percentage);
+iCommsMessage_t IComms_CreatePercentageMessage(uint16_t standardMessageID, percentage_t percentage);
 
-PUBLIC iCommsMessage_t IComms_CreateUint32BitMessage(uint16_t standardMessageID, uint32_t value);
+iCommsMessage_t IComms_CreateUint32BitMessage(uint16_t standardMessageID, uint32_t value);
 
-PUBLIC iCommsMessage_t IComms_CreateInt32BitMessage(uint16_t standardMessageID, int32_t value);
+iCommsMessage_t IComms_CreateInt32BitMessage(uint16_t standardMessageID, int32_t value);
 
-PUBLIC iCommsMessage_t IComms_CreateErrorMessage(uint16_t standardMessageID, ErrorCode code, flag_status_t);
+/**
+ * 4 bits -> 16 devices
+ * 4 bits -> 16 fault categories
+ * 24 bits -> data mask
+ * 32 bits -> pointer
+ * @param standardMessageID
+ * @param code
+ * @return
+ */
+iCommsMessage_t IComms_CreateErrorMessage(uint8_t device, uint8_t code, uint32_t flags, uint32_t data);
 
-PUBLIC iCommsMessage_t IComms_CreateEventMessage(uint16_t standardMessageID, uint8_t code, uint8_t status);
+iCommsMessage_t IComms_CreateEventMessage(uint8_t code, uint8_t status);
 
-PUBLIC iCommsMessage_t IComms_CreatePairUInt16BitMessage(uint16_t standardMessageID, uint16_t a, uint16_t b);
+iCommsMessage_t IComms_CreatePairUInt16BitMessage(uint16_t standardMessageID, uint16_t a, uint16_t b);
 
-PUBLIC iCommsMessage_t IComms_CreateLightsMessage(uint16_t standardMessageID, uint8_t code, uint8_t status);
+iCommsMessage_t IComms_CreateLightsMessage(lights_status_t lights);
 
-PUBLIC iCommsMessage_t IComms_CreatePairInt32Message(uint16_t standardMessageID, int32_t a, int32_t b);
-PUBLIC result_t IComms_ReadPairInt32Message(iCommsMessage_t *msg, int32_t* a, int32_t* b);
+iCommsMessage_t IComms_CreatePairInt32Message(uint16_t standardMessageID, int32_t a, int32_t b);
+result_t IComms_ReadPairInt32Message(iCommsMessage_t *msg, int32_t* a, int32_t* b);
 
-PUBLIC iCommsMessage_t IComms_CreatePressureTemperatureMessage(uint16_t standardMessageID, pressure_t a, temperature_t b);
-PUBLIC result_t IComms_ReadPressureTemperatureMessage(iCommsMessage_t* msg, pressure_t* a, temperature_t* b);
+iCommsMessage_t IComms_CreatePressureTemperatureMessage(pressure_t a, temperature_t b);
+result_t IComms_ReadPressureTemperatureMessage(iCommsMessage_t* msg, pressure_t* a, temperature_t* b);
 
 /**
  * Create an efficiency message that packs 4 laps into a CAN message
@@ -50,20 +59,19 @@ PUBLIC result_t IComms_ReadPressureTemperatureMessage(iCommsMessage_t* msg, pres
  * @param efficiencies
  * @return
  */
-PUBLIC iCommsMessage_t IComms_CreateEfficiencyMessage(uint16_t standardMessageID, lap_efficiencies_t* efficiencies);
+iCommsMessage_t IComms_CreateEfficiencyMessage(lap_efficiencies_t* efficiencies);
 
-PUBLIC result_t IComms_ReadEfficiencyMessage(iCommsMessage_t *msg, lap_efficiencies_t* result);
+result_t IComms_ReadEfficiencyMessage(iCommsMessage_t *msg, lap_efficiencies_t* result);
 
 typedef union {
     uint32_t all;
     struct {
         uint16_t a:16;
         uint16_t b:16;
-        uint32_t reserved;
     };
 } uint16_pair_t;
 
-PUBLIC uint16_pair_t readMsgPairUInt16Bit(iCommsMessage_t *msg);
+uint16_pair_t readMsgPairUInt16Bit(iCommsMessage_t *msg);
 
 #ifdef __cplusplus
 }
