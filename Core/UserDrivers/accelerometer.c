cfcf68fa541ea0f73e598de7bbeb93cf68494cee
Taha Rashid <taharashid710192@gmail.com>
2025-02-02 08:46:41 -0500

    Created accelerometer.c

--- Core/UserDrivers/accelerometer.c
+++ Core/UserDrivers/accelerometer.c
@@ -2,5 +2,11 @@
 // Created by Taha Rashid on 2024-11-19.
 //
 
-//this file talks to the hardware, Tasks/Accelerometer.c uses it
-//look at current_sensor as an example
+// Communicates with the accelerometer to fetch data
+
+/* PLANS:
+ * we want to fetch data from accelerometer -> read documentation
+ * probably want to implement start(), read_data() methods
+ * look at current_sensor as an example
+ * using i2c, supports speeds
+*/
