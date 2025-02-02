412ae03bc1dca0f76e521da675621b5be41752b5
Taha Rashid <taharashid710192@gmail.com>
2025-02-02 09:07:07 -0500

    Added additional plans to TODO list

--- Core/UserDrivers/accelerometer.c
+++ Core/UserDrivers/accelerometer.c
@@ -4,8 +4,9 @@
 
 // Communicates with the accelerometer to fetch data
 
-/* PLANS:
+/* TODO:
  * we want to fetch data from accelerometer -> read documentation
+ * look at old implementation in accelerometer_adafruit.c
  * probably want to implement start(), read_data() methods
  * look at current_sensor as an example
  * using i2c, supports speeds
