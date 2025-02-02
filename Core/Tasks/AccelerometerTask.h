5c5ff77ed4d41986ece0ac776343c2e6f9e434fc
Taha Rashid <taharashid710192@gmail.com>
2025-02-02 08:58:18 -0500

    Changed the name of the task function to match naming conventions across project

---
+++ Core/Tasks/AccelerometerTask.h
@@ -0,0 +1,10 @@
+//
+// Created by Taha Rashid on 2024-11-19.
+//
+
+#ifndef UOSM_TELEMETRY_PICO_ACCELEROMETER_H
+#define UOSM_TELEMETRY_PICO_ACCELEROMETER_H
+
+void InitAccelerometerTask();
+
+#endif //UOSM_TELEMETRY_PICO_ACCELEROMETER_H
