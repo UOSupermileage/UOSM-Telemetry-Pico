a24849ded5aed7b23754af284c6818f3dec86fe2
Taha Rashid <taharashid710192@gmail.com>
2025-02-08 13:51:42 -0500

    chore: declare functions to access accelerometer data for future implementation

--- Core/Inc/accelerometer.h
+++ Core/Inc/accelerometer.h
@@ -2,7 +2,16 @@
 // Created by Taha Rashid on 2024-11-19.
 //
 
-#ifndef ACCELEROMETER_H
-#define ACCELEROMETER_H
+#ifndef UOSM_TELEMETRY_PICO_ACCELEROMETER_H
+#define USOM_TELEMETRY_PICO_ACCELEROMETER_H
 
-#endif //ACCELEROMETER_H
+// include enum header ApplicationTypes.h if needed
+
+// declaring functions that will be used in AccelerometerTask, and implemented in accelerometer.c
+// init, start, is_data_ready, read_conversion
+bool accelerometer_init();
+bool accelerometer_start();
+bool accelerometer_is_data_ready();
+bool accelerometer_read_acceleration(float* x, float* y, float* z);
+
+#endif //USOM_TELEMETRY_PICO_ACCELEROMETER_H
