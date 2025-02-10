ac8827d3e47eb27788baf05b47a6f8cdd43d5c95
Taha Rashid <taharashid710192@gmail.com>
2025-02-10 09:27:49 -0500

    feat: added write_bytes, write_byte, write_register functions
    Using these functions, other accelerometer functions are starting to be implemented

--- Core/UserDrivers/accelerometer.c
+++ Core/UserDrivers/accelerometer.c
@@ -9,6 +9,7 @@
 
 #include <FreeRTOS.h>
 #include <task.h>
+#include <hardware/gpio.h>
 
 // I2C info
 // TODO: Set the correct timeout
@@ -27,32 +28,94 @@ const uint8_t iim42653_reg_config_accel0 = 0x00;
 const uint8_t iim42653_reg_config_accel1 = 0x00;
 
 // registers where the accelerometer data is
-const uint8_t iim42653_read_accel_x1 = 0x1F;
-const uint8_t iim42653_read_accel_x0 = 0x20;
-const uint8_t iim42653_read_accel_y1 = 0x21;
-const uint8_t iim42653_read_accel_y0 = 0x22;
-const uint8_t iim42653_read_accel_z1 = 0x23;
-const uint8_t iim42653_read_accel_z0 = 0x24;
+const uint8_t iim42653_reg_accel_x1 = 0x1F;
+const uint8_t iim42653_reg_accel_x0 = 0x20;
+const uint8_t iim42653_reg_accel_y1 = 0x21;
+const uint8_t iim42653_reg_accel_y0 = 0x22;
+const uint8_t iim42653_reg_accel_z1 = 0x23;
+const uint8_t iim42653_reg_accel_z0 = 0x24;
 
 // TODO: Check if we need this
 const uint8_t iim42653_read_whoami = 0x75;
 
-// device config
-static bool configure_device()
+// TODO: create functions for read_register() (to be universially used)
+// writes multiple bytes. This is a util method (other functions use this method)
+static bool write_bytes(uint8_t* bytes, size_t len, bool end_of_transmission)
 {
- // just sending 1 byte
- const int len = 1;
- // TODO: set the config data we want to set
- const uint8_t config_device_data = 0x00;
-
- int result = i2c_write_timeout_us(I2C_INSTANCE, I2C_ADDRESS, &config_device_data, len, false, I2C_TIMEOUT);
+ int result = i2c_write_timeout_us(I2C_INSTANCE, I2C_ADDRESS, bytes, len, end_of_transmission , I2C_TIMEOUT);
  return result == len;
 }
 
-// accel config
-void configure_accel() {}
+// writes a single byte
+static bool write_byte(uint8_t byte, bool end_of_transmission)
+{
+ const uint8_t buffer[1] = { byte };
+ return write_bytes(buffer, 1, end_of_transmission);
+}
 
+// writes to a register
+static bool write_register(uint8_t reg, uint8_t data)
+{
+ const uint8_t buffer[2] = { reg, data };
+ return write_bytes(buffer, 2, true);
+}
 
+// device config, using default config
+static bool configure_device(uint8_t config_device_data)
+{
+ // default data for config_device_data is 0x00
+ return  write_register(iim42653_reg_config_device, config_device_data);
+}
+
+// accel config, using default config
+static bool configure_acceleromter(config_device_data)
+{
+ // default data for config_device_data is 0x06
+ return write_register(iim42653_reg_config_device, config_device_data);
+}
+
+bool accelerometer_reset()
+{
+ // resets device
+ return configure_device(0x01);
+}
+
+void accelerometer_begin()
+{
+ accelerometer_reset(); // TODO: Check return
+
+ // TODO: Check if sensor is ready to be read
+ // code...
+
+ // TODO: Check return
+ // could set full-scale select, ODR for accel, etc.
+ configure_device(0x00);
+ configure_acceleromter(0x06);
+
+ // returns bool
+}
+
+void accelerometer_init()
+{
+ // sets GPIO pins, I2C, and runs accelerometer_begin()
+ // look at accelerometer_adafruit.c for example
+
+ //gpio_init(), gpio_set_dir(), gpio_pull_up()?
+
+ // TODO: find correct baud rate
+ i2c_init(i2c_default, 10000);
+ gpio_set_function(I2C_SDA_PIN, GPIO_FUNC_I2C);
+ gpio_set_function(I2C_SCL_PIN, GPIO_FUNC_I2C);
+
+ // return accelerometer_begin()
+}
+
+void accelerometer_start()
+{
+ // sends command to start receiving data
+ // configure_device(something here?)
+ // returns bool
+}
 
 /* TODO:
  * we want to fetch data from accelerometer -> read documentation
